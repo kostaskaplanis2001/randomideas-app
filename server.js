79347fc5ab53c584d2f1d64cacab10fcb41677bc
Kostas Kaplanis <kostaskaplanis2001@hotmail.com>
2024-04-04 22:00:13 +0300

    Added REST API functionalities

--- server.js
+++ server.js
@@ -1,43 +1,17 @@
 const express = require('express');
-const port = 5011;
+const port = 5012;
 
 const app = express();
 
-const ideas = [
-  {
-    id: 1,
-    text: 'Positive NewsLetter, a newsletter that only shares positive, uplifting news',
-    tag: 'Technology',
-    username: 'TonyStark',
-    date: '2022-01-02',
-  },
-  {
-    id: 2,
-    text: 'Milk cartons that turn a different color the older that your milk is getting',
-    tag: 'Inventions',
-    username: 'SteveRogers',
-    date: '2022-01-02',
-  },
-  {},
-];
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the RandomIdeas API' });
 });
 
-// Get all ideas
-app.get('/api/ideas', (req, res) => {
-  res.json({ success: true, data: ideas });
-});
-
-app.get('/api/ideas/:id', (req, res) => {
-  const idea = ideas.find((idea) => idea.id === +req.params.id);
-  if (!idea) {
-    return res
-      .status(404)
-      .json({ success: false, error: 'Resource not found' });
-  }
-  res.json({ success: true, data: idea });
-});
+const ideasRouter = require('./routes/ideas');
+app.use('/api/ideas', ideasRouter);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
