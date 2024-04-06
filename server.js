0f12e7d4da257eb8d641aabf89ff5968480d0c19
Kostas Kaplanis <kostaskaplanis2001@hotmail.com>
2024-04-06 17:06:23 +0300

    I just made randomideas-api to a complete fullstack app ready for deployment

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
+const path = require('path');
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
-const port = process.env.PORT || 5012;
+const port = process.env.PORT || 5011;
 const connectDB = require('./config/db');
 
 connectDB();
 
 const app = express();
 
-//Body parser middleware
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// cors middleware
+app.use(
+  cors({
+    origin: ['http://localhost:5011', 'http://localhost:3000'],
+    credentials: true,
+  })
+);
 
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the RandomIdeas API' });
